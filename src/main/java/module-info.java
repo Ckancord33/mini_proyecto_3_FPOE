24b6d107c873f7a8798fc09a5cbb8bfb8ac9b8f9
Ckancord33 <niccord33@gmail.com>
2024-10-29 22:59:49 -0500

    Primer commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module com.example.miniproyecto_3_battlership {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.miniproyecto_3_battlership to javafx.fxml;
+    exports com.example.miniproyecto_3_battlership;
+    exports com.example.miniproyecto_3_battlership.controller;
+    opens com.example.miniproyecto_3_battlership.controller to javafx.fxml;
+}
