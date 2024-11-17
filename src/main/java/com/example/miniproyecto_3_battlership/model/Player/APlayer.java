23e8253533326974d6a1742f676e234a55b81d1e
Samuel Arenas <samuel.arenas@correounivalle.edu.co>
2024-11-16 20:00:27 -0500

    Se añadio el diseño de la fragata, animacion para el dado de ramdon

---
+++ src/main/java/com/example/miniproyecto_3_battlership/model/Player/APlayer.java
@@ -0,0 +1,47 @@
+package com.example.miniproyecto_3_battlership.model.Player;
+
+import java.util.ArrayList;
+
+public abstract class APlayer implements IPlayer {
+    @Override
+    public void setMatrix(){
+
+    }
+
+    @Override
+    public ArrayList<ArrayList<Integer>> getMatrix(){
+      return null;
+    }
+
+    @Override
+    public void clearMatrix(){
+
+    }
+
+    @Override
+    public void changeValuesMatrix(int[][] matrix){
+
+    }
+
+    @Override
+    public void setChosenMatrix(int[][] matrix){
+
+    }
+
+    @Override
+    public void generateBotGame(){
+
+    }
+
+    @Override
+    public void botIntelligence(){
+
+    }
+
+    @Override
+    public void gameTurnPlayer(){
+
+    }
+
+
+}
