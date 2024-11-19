e700ccaad39f679c4956b6843f792728b44fbcb4
Juan Ampudia <juan.ampudia@correounivalle.edu.co>
2024-11-19 17:44:26 -0500

    Diseño del destructor

--- src/main/java/com/example/miniproyecto_3_battlership/model/ships/Submarino.java
+++ src/main/java/com/example/miniproyecto_3_battlership/model/ships/Submarino.java
@@ -1,8 +1,16 @@
 package com.example.miniproyecto_3_battlership.model.ships;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.image.Image;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
+import javafx.scene.paint.ImagePattern;
+import javafx.scene.shape.*;
+
+import java.util.Collection;
+import java.util.Objects;
 
 public class Submarino extends Ship {
 
@@ -10,13 +18,44 @@ public class Submarino extends Ship {
         this.setWidth(190);
         this.setHeight(63);
         size =  3;
-        body = new Rectangle(0, 0, 190, 63);
-        body.setFill(javafx.scene.paint.Color.DARKGRAY);
-        body.setStroke(Color.TRANSPARENT);
-        this.getChildren().add(body);
+        Ellipse ellipse = new Ellipse();
+        ellipse.setCenterX(90); // Posición X del centro
+        ellipse.setCenterY(25); // Posición Y del centro
+        ellipse.setRadiusX(60); // Radio horizontal
+        ellipse.setRadiusY(25);  // Radio vertical
+        ellipse.setFill(javafx.scene.paint.Color.GRAY);
+
+        Image image = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/com/example/miniproyecto_3_battlership/Image/texture2.png")));
+        ImagePattern imagePattern = new ImagePattern(image);
+        ellipse.setFill(imagePattern);
+        ellipse.setStroke(Color.BLACK);
+        Polygon polygon = new Polygon();
+
+
+
+
+
+
+
+
+        DropShadow glow = new DropShadow();
+        glow.setColor(Color.CADETBLUE);
+        glow.setRadius(10);
+        glow.setSpread(0.3);
+        shapesGroup.setEffect(glow);
+        shapesGroup.setStyle("-fx-cursor: hand");
+        shapesGroup.setLayoutX(10);
+        shapesGroup.setLayoutY(2);
+        shapesGroup.setScaleX(1.1);
+        shapesGroup.setScaleY(1.1);
+
+        shapesGroup.getChildren().addAll(ellipse);
+
+        shapesGroup.setScaleX(1.0);
+        shapesGroup.setScaleY(1.0);
+        this.getChildren().add(shapesGroup);
         isSelect = false;
         size = 3;
-        isHorizontal = true;
     }
 
     public Submarino(boolean b) {
